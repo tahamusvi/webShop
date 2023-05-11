7743de69bde3aa9b325ed33a3cd911e9dd3a805c
taa <TahaM8000@gmail.com>
2023-05-12 02:03:44 +0430

    add discount stuff part in landing page

--- facades/views.py
+++ facades/views.py
@@ -5,8 +5,9 @@ from stuff.models import Product
 #----------------------------------------------------------------------------------------------
 def HomePage(request):
     print("--------------------------")
+    products2 = Product.objects.filter(available=True)
     products = Product.objects.filter(available=True)
 
     # return render(request,'facades/landing.html')
-    return render(request,'facades/landing.html',{'products':products,})
+    return render(request,'facades/landing.html',{'products':products,'productsOfDiscount':products2,})
 #----------------------------------------------------------------------------------------------
