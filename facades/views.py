77757f9df5f2e137857cb30460303732613dbceb
taa <TahaM8000@gmail.com>
2023-05-18 00:00:37 +0330

    edit footer

--- facades/views.py
+++ facades/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from stuff.models import Product
+from stuff.models import Product,Category
 
 
 #----------------------------------------------------------------------------------------------
@@ -11,12 +11,15 @@ def HomePage(request):
     # New stuff in website
     news = Product.objects.filter(available=True)
     news_list = [s.id for s in news if s.is_new][0:8]
-
     news = news.filter(id__in=news_list)
+
+    # Categories
+    Categories = Category.objects.filter(is_sub=False)[0:4]
+
 
 
 
 
     # return render(request,'facades/landing.html')
-    return render(request,'facades/landing.html',{'products':news,'productsOfDiscount':discounted,})
+    return render(request,'facades/landing.html',{'products':news,'productsOfDiscount':discounted,'Categories' : Categories,})
 #----------------------------------------------------------------------------------------------
