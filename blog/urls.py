f55d9fede847219fbc3d404466f889efc9e9f980
taa <TahaM8000@gmail.com>
2023-09-10 20:30:41 +0330

    detail post page

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path,include
 from .views import *
 
-
 app_name = "blog"
 urlpatterns = [    
+
+    # path('',PostListView.as_view(),name="home"),
+    # path('<int:page>/',PostListView.as_view(),name = "home"),
+    path('posts/<slug>/',PostDetail.as_view(),name = "post"),
+    # path('Authors/<slug:phoneNumber>',AuthorDetail.as_view(),name = "author"),
+    # path('Authors/<slug:phoneNumber>/<int:page>',AuthorDetail.as_view(),name = "author"),
+    # path('Category/<slug>',CategoryDetail.as_view(),name = "category"),
+    # path('Category/<slug>/<int:page>',CategoryDetail.as_view(),name = "category"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from accounts.models import User
+#----------------------------------------------------------------------------------------------
+# class PostListView(ListView):
+#     model = models.Post
+#     context_object_name = "Posts"
+#     template_name = "posts/classBaseViews/home.html"
+#     queryset = models.Post.objects.published()[::-1]
+#     paginate_by = 5
+#----------------------------------------------------------------------------------------------
+class PostDetail(DetailView):
+    template_name = "blog/Post_detail.html"
+    context_object_name = "post"
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        return get_object_or_404(Post.objects.published(), slug=slug)
+#----------------------------------------------------------------------------------------------
+# class AuthorDetail(ListView):
+#     paginate_by = 3
+#     template_name = "posts/classBaseViews/Author_detail.html"
 
-# Create your views here.
+#     def get_queryset(self):
+#         global Author
+#         phoneNumber = self.kwargs.get('phoneNumber')
+#         Author = User.objects.get(phoneNumber=phoneNumber)
+#         return Author.posts.published()[::-1]
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['Author'] = Author
+#         return context
+#----------------------------------------------------------------------------------------------
+# class CategoryDetail(ListView):
+#     paginate_by = 4
+#     template_name = "posts/classBaseViews/Category_detail.html"
+
+#     def get_queryset(self):
+#         global Category
+#         slug = self.kwargs.get('slug')
+#         Category = get_object_or_404(models.Category.objects.actived(), slug=slug)
+#         return Category.posts.published()
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['Category'] = Category
+#         return context
+#----------------------------------------------------------------------------------------------
