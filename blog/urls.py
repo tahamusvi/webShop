b1886e1c4e6c77ac916449b75dcb191a7a559550
taa <TahaM8000@gmail.com>
2023-09-16 03:09:49 +0330

    update blog:home to home_blog - fix some bugs - add blog href to mobile section

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 app_name = "blog"
 urlpatterns = [    
-    path('',post_list,name="home"),
-    path('<int:page>/',post_list,name="home"),
+    path('',post_list,name="home_blog"),
+    path('<int:page>/',post_list,name="home_blog"),
     path('posts/<slug>/',post_detail,name="post"),
     path('authors/<int:id>/<int:page>/',author_posts,name = "author_posts"),
     path('categories/<slug>/<int:page>/',category_posts,name = "category_posts"),
