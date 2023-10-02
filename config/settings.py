f282c4b887f7df1dd2ebbe6bd7a651ad9413155e
taa <TahaM8000@gmail.com>
2023-10-02 05:36:02 +0330

    update settings for db

--- config/settings.py
+++ config/settings.py
@@ -80,12 +80,25 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+if(deploy):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('database_name', 'LIARA_URL is not set.'),
+            'USER': os.getenv('database_username', 'LIARA_URL is not set.'),
+            'PASSWORD': os.getenv('password', 'LIARA_URL is not set.'),
+            'HOST': os.getenv('database_hostname_or_ip', 'LIARA_URL is not set.'),
+            'PORT': os.getenv('database_port', 'LIARA_URL is not set.'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
