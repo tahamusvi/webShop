17226bd95cf17dc618873295a2a67540d72b6950
taa <TahaM8000@gmail.com>
2023-10-15 22:25:45 +0330

    change title

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('youCantFindIt/', admin.site.urls),
     path('', include('facades.urls')),
     path('', include('stuff.urls')),
     path('accounts/', include('accounts.urls')),
