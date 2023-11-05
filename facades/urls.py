e22af58b2df9093cc41df1592b53246a5aaeb082
Taha <TahaM8000@gmail.com>
2023-11-06 02:08:24 +0330

    new home page

--- facades/urls.py
+++ facades/urls.py
@@ -4,7 +4,8 @@ from .views import *
 app_name = "facades"
 
 urlpatterns = [
-    path('',HomePage,name="home"),
+    # path('',HomePage,name="home"),
+    path('',HomePage2,name="home"),
     path('contact/',contact,name="contact"),
     path('CreateSurvey/',CreateSurvey,name="CreateSurvey"),
     path('aboutUs/',aboutUs,name="aboutUs"),
