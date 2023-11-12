d371103014f93c53980586a6e09f2789302048b0
taa <TahaM8000@gmail.com>
2023-11-12 21:59:52 +0330

    another image product

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-deploy = False
+deploy = True
 if(deploy):
     # deploy
     SECRET_KEY = os.getenv('SECRET_KEY', 'LIARA_URL is not set.')
