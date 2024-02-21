9cc0c28f04e6d939622415a11ffe2a463a0d455c
taa <TahaM8000@gmail.com>
2024-02-21 13:23:58 +0330

    other image for product

--- stuff/admin.py
+++ stuff/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import *
+from django.utils.html import format_html
+#-------------------------------------------------------------------------
+class ProductImageItemInline(admin.TabularInline):
+    model = ProductImage
+    raw_id_fields = ('product',)
+    fields = ('alt', 'is_main','image','image_tag')
+    readonly_fields = ('image_tag',)
 
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" style="width: 50px; height: auto;" />'.format(obj.image.url))
+
+    image_tag.short_description = 'Image'
+#-------------------------------------------------------------------------
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name','slug')
@@ -8,12 +21,14 @@ class CategoryAdmin(admin.ModelAdmin):
 #-------------------------------------------------------------------------
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name','price','available','updated','is_new')
+    list_display = ('name','image_tag','price','available','updated','is_new')
+    search_fields = ['name', 'description',]
     prepopulated_fields = {'slug':('name',)}
     list_filter = ('available','created')
     list_editable = ('price',)
     raw_id_fields = ('category',)
-    actions = ('make_available','make_unavailable')
+    actions = ('make_available','make_unavailable','add_specific_color')
+    inlines = (ProductImageItemInline,)
 
     def make_available(self,request,queryset):
         rows = queryset.update(available=True)
@@ -23,8 +38,19 @@ class ProductAdmin(admin.ModelAdmin):
         rows = queryset.update(available=False)
         self.message_user(request,f'{rows} updated')
 
+    def add_specific_color(self, request, queryset):
+        color = Color.objects.get(en='custom')
+        for product in queryset:
+            product.colors.add(color)
+        self.message_user(request, f'The specific color has been added to selected products.')
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" style="width: 50px; height: auto;" />'.format(obj.image.url))
+
+    image_tag.short_description = 'Image'
 
     make_available.short_description = 'make all available'
+    add_specific_color.short_description = 'Add specific color to selected products'
 #-------------------------------------------------------------------------
 admin.site.register(Brand)
 admin.site.register(ProductImage)
